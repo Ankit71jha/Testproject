c9213e63966249e91f7a28b75aaaaa18d6569f0b
M1082690 <M1082690@MT-5CG14791N6>
2022-04-26 12:03:09 +0530

    Add2

--- TestProject/src/TestProject/Test1.java
+++ TestProject/src/TestProject/Test1.java
@@ -1,6 +1,8 @@
 package TestProject;
 
 public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
 
-
+}
 }
--- TestProject/src/TestProject/Test2.java
+++ TestProject/src/TestProject/Test2.java
@@ -1,5 +1,8 @@
 package TestProject;
 
 public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
 
+}
 }
